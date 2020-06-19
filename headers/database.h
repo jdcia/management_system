1a7841b4d4f376305f14f688dcd83099be79d6ff
jacob <jacob.garcia@student.nmt.edu>
2020-06-18 21:42:28 -0600

    added class database, also created a employee table in management.db

---
+++ headers/database.h
@@ -0,0 +1,25 @@
+#ifndef PROCESS_H
+#define PROCESS_h
+
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+
+
+
+
+class database {
+
+    public:
+
+        database(string path);
+
+
+    private:
+
+
+};
+
+#endif
